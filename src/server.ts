aa10a4fc3a59bc5ebfadf93421685babf0d870b5
Mohsen Bostan <mohsenbostandev@gmnail.com>
2021-08-25 00:24:59 +0430

    init commit :rocket:

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import env from 'dotenv';
+import * as SpamDetection from 'spam-detection';
+import Discord from 'discord.js';
+import { ILogObject, Logger } from 'tslog';
+import { appendFileSync } from "fs";
+
+env.config();
+
+const logToTransport = (logObject: ILogObject) => {
+    appendFileSync("logs.txt", `${logObject.date} : ${logObject.argumentsArray[0]} \n`);
+}
+
+const log: Logger = new Logger({
+    type: 'json',
+});
+log.attachTransport(
+    {
+        silly: logToTransport,
+        debug: logToTransport,
+        trace: logToTransport,
+        info: logToTransport,
+        warn: logToTransport,
+        error: logToTransport,
+        fatal: logToTransport,
+    },
+    "info"
+);
+
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+client.on('messageCreate', message => {
+    const spamDetectionResult = SpamDetection.getResults(message.content).filter((x: { label: string, value: number }) => x.label === 'spam');
+    if (spamDetectionResult.length > 0) {
+        const spamDetectionValue = parseFloat(spamDetectionResult[0].value.toString().slice(0, 4));
+        if (spamDetectionValue > 4 ||
+            (message.content.includes('@everyone') && spamDetectionValue > 2)) {
+            message.delete();
+            //@ts-ignore
+            log.info(`Deleted message from ${message.author.username} in ${message.guild?.name} - ${message.channel.name}`);
+        }
+    }
+});
---
+++ src/types.d.ts
@@ -0,0 +1,1 @@
+declare module 'spam-detection';
